5cbc13cf7c6037dac4f759c664d53a47bf14a0d8
whiz-dev <whiz123@hanmail.net>
2022-02-15 16:50:37 +0900

    createBulkTodos 구현 추가

--- src/App.js
+++ src/App.js
@@ -3,24 +3,22 @@ import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
-const App = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '리택트의 기초 알아보기',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: '컴포넌트 스타일링해 보기',
-      checked: true,
-    },
-    {
-      id: 3,
-      text: '일정 관리 앱 만들어 보기',
+function createBulkTodos() {
+  const array = [];
+
+  for (let i = 0; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: '할 일' + i,
       checked: false,
-    },
-  ]);
+    });
+  }
+
+  return array;
+}
+
+const App = () => {
+  const [todos, setTodos] = useState(createBulkTodos);
 
   // 고윳값으로 사용될 id
   // ref를 사용하여 변수 담기
